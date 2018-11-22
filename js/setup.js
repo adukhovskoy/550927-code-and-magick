66b3d7e0b07e52ee8d9a18cd94fadc5ae6e8b50a
Aleksandr Dukhovskoy <alexdoohee@gmail.com>
2018-11-22 22:32:40 +0600

    add generated wizards to DOM

--- js/setup.js
+++ js/setup.js
@@ -6,9 +6,19 @@ var MOCK_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100
 var MOCK_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_COUNT = 4;
 
-function getRandomInRange(min, max) {
+// вот тут не уверен, может быть проще было завадать сразу '#id' '.class'
+// т.к. дальше приходится делать такое: querySelector('#' + templateID)
+var WIZARD_TEMPLATE_ID = 'similar-wizard-template';
+var WIZARDS_LIST_CLASS = 'setup-similar-list';
+
+var getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+
+// не избыточно ли? можно каждый раз писать getRandomInRange(0, array / length - 1) а не использовать функцию
+var getRandomIndex = function (array) {
+  return getRandomInRange(0, array.length - 1);
+};
 
 var showElement = function (selector) {
   var element = document.querySelector(selector);
@@ -17,17 +27,42 @@ var showElement = function (selector) {
 
 var generateMockWizards = function (names, surNames, coatColors, eyesColors, count) {
   var mockWizards = [];
+  var fullName = '';
   for (var i = 0; i < count; i++) {
+    if (getRandomInRange(0, 1) === 0) {
+      fullName = names[getRandomIndex(names)] + ' ' + surNames[getRandomIndex(surNames)];
+    } else {
+      fullName = surNames[getRandomIndex(surNames)] + ' ' + names[getRandomIndex(names)];
+    }
     var mockWizard = {
-      name: names[getRandomInRange(0, names.length - 1)] + ' ' + surNames[getRandomInRange(0, surNames.length - 1)],
-      coatColor: coatColors[getRandomInRange(0, coatColors.length - 1)],
-      eyesColor: eyesColors[getRandomInRange(0, eyesColors.length - 1)]
+      name: fullName,
+      coatColor: coatColors[getRandomIndex(coatColors)],
+      eyesColor: eyesColors[getRandomIndex(eyesColors)]
     };
     mockWizards.push(mockWizard);
   }
   return mockWizards;
 };
 
+var generateWizardElement = function (templateID, wizard) {
+  var template = document.querySelector('#' + templateID).content.querySelector('div');
+  var element = template.cloneNode(true);
+  element.querySelector('.setup-similar-label').textContent = wizard.name;
+  element.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  element.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return element;
+};
+
+var fillWizardsList = function (wizardsListClass, wizardTemplateID, wizards) {
+  var fragment = document.createDocumentFragment();
+  var wizardsList = document.querySelector('.' + wizardsListClass);
+  for (var l = 0; l < wizards.length; l++) {
+    fragment.appendChild(generateWizardElement(wizardTemplateID, wizards[l]));
+  }
+  wizardsList.appendChild(fragment);
+};
+
 showElement('.setup');
-generateMockWizards(MOCK_NAMES, MOCK_SURNAMES, MOCK_COAT_COLORS, MOCK_EYES_COLORS, WIZARDS_COUNT);
+var generatedWizards = generateMockWizards(MOCK_NAMES, MOCK_SURNAMES, MOCK_COAT_COLORS, MOCK_EYES_COLORS, WIZARDS_COUNT);
+fillWizardsList(WIZARDS_LIST_CLASS, WIZARD_TEMPLATE_ID, generatedWizards);
 showElement('.setup-similar');
